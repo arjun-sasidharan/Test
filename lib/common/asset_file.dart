f0e83203490d3a6e76f64229f7d74a6ead94e466
Arjun <71964107+arjun-sasidharan@users.noreply.github.com>
2023-12-23 00:26:32 +0530

    Add font, icon assets

--- lib/common/asset_file.dart
+++ lib/common/asset_file.dart
@@ -1,1 +1,5 @@
-const searchIcon = 'assets/icons/ic_search.svg';
+const iconSearch = 'assets/icons/ic_search.svg';
+const iconScanLine = 'assets/icons/ic_scan_line.svg';
+const iconOffice = 'assets/icons/ic_office_bag.svg';
+const iconChat = 'assets/icons/ic_chat.svg';
+const iconAccount = 'assets/icons/ic_account.svg';
---
+++ lib/common/constants.dart
@@ -0,0 +1,5 @@
+
+import 'package:flutter/material.dart';
+
+const yellowColor = Color(0xFFFAAA14);
+const textColor = Color(0xFF303030);
